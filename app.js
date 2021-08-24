163ca2fbbdff83fe7070a709dd97f8b2ece447be
Mason-GR <mase210@gmail.com>
2021-08-24 19:51:23 -0400

    added notes

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 let timer = setInterval(move, 100);
 
+// Boxes starting position
 let boxPosition = 0;
-
+// Select the box by it's ID
 let box = document.getElementById("box");
 
 function move() {
   if (boxPosition >= 175) {
     clearInterval(timer);
   } else {
-    boxPosition += 1;
+    boxPosition += 1; // Imcreases the boxPosition value by 1
     box.style.left = boxPosition + "px";
+    // Updates box's position from the left of the container
   }
 }
