5a3255f268a2ddad49e7c9f056bf4ef2634117b0
Mason-GR <mase210@gmail.com>
2021-08-24 13:38:06 -0400

    added moving box example

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+let timer = setInterval(move, 100);
 
+let boxPosition = 0;
+
+let box = document.getElementById("box");
+
+function move() {
+  if (boxPosition >= 175) {
+    clearInterval(timer);
+  } else {
+    boxPosition += 1;
+    box.style.left = boxPosition + "px";
+  }
+}
