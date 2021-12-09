8cb8c0546177ca2ffd629fa7f87cdf5f345d85e8
Mason-GR <mase210@gmail.com>
2021-12-08 21:45:42 -0500

    added setTimeout modal

--- app.js
+++ app.js
@@ -14,3 +14,23 @@ function move() {
     // Updates box's position from the left of the container
   }
 }
+
+let input = document.getElementById("message");
+// selecting by ID's
+let title = document.getElementById("title");
+
+input.addEventListener("keyup", function (event) {
+  title.textContent = event.target.value;
+}); // assigning a listener to change text on typing with a function
+
+let modal = document.getElementsByClassName("modal")[0];
+
+setTimeout(function () {
+  modal.style.display = "flex";
+}, 5000);
+
+document
+  .getElementsByClassName("modal-btn")[0]
+  .addEventListener("click", function () {
+    modal.style.display = "none";
+  });
